4e32c8a2d409e2902fa03a125e7b00dec7bd8a60
Victor Daniel <70461321+Victor07july@users.noreply.github.com>
2022-02-25 18:03:11 -0300

    Add files via upload

---
+++ components/Spacer.js
@@ -0,0 +1,15 @@
+//Componente para aplicar uma margem em um componente
+import React from 'react';
+import { View, StyleSheet } from 'react-native'
+
+const Spacer = ({ children }) => {
+    return <View style={styles.spacer}>{children}</View>
+};
+
+const styles = StyleSheet.create({
+    spacer: {
+        margin: 15
+    }
+});
+
+export default Spacer;
